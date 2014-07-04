30e7c80af2189db65f83bc7478e162e360d92082
Thomas <txchen@gmail.com>
2014-07-03 20:23:18 -1000

    minor adjustment on format: use 4 spaces to replace tab

--- BirthdayCalculator/src/com/xc/hawaii/birthdaycalculator/Util.java
+++ BirthdayCalculator/src/com/xc/hawaii/birthdaycalculator/Util.java
@@ -18,82 +18,82 @@ import android.util.Log;
  * 
  */
 public class Util extends ActionBarActivity  {
-	private static boolean DEBUG = false;
-	public static String CONST_SharedPrefName = "BirthdayCalculator";
-	
-	public Util() {
-		// Get debug variable from values/strings.xml.
-	}
-	
-	public static void setDebugMode(boolean DebugMode) { DEBUG = DebugMode; }
-	public static boolean getDebugMode() { return DEBUG; }
-	
-	/**
-	 * Display a dialog.
-	 * 
-	 * @param strTitle
-	 * @param strErr
-	 * @param me
-	 */
-	public static void showDialog(String strTitle, String strErr, ActionBarActivity me) {
-		AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(me); 
-		alertDialogBuilder.setTitle(strTitle);
-		alertDialogBuilder.setMessage(strErr);
-		alertDialogBuilder.setCancelable(false);
-		alertDialogBuilder.setPositiveButton("OK",
-				new DialogInterface.OnClickListener() {						
-					@Override
-					public void onClick(DialogInterface dialog, int which) {
-						//MainActivity.this.finish();
-						dialog.cancel();
-					}
-				});
-		
-		AlertDialog alertDialog = alertDialogBuilder.create();
-		
-		alertDialog.show();		
-	}
-	
-	/**
-	 * This dialog needs a layout xml. To be implemented.
-	 * 
-	 * @param strTitle
-	 * @param strErr
-	 * @param me
-	 */
-	public static void showDialog2(String strTitle, String strErr, ActionBarActivity me) {
-		final Dialog dialog = new Dialog(me);
-		//dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
-		
-		dialog.setTitle(strTitle);
-		dialog.setContentView(R.layout.activity_setting);
-		Drawable d = new ColorDrawable(Color.BLACK); 
-		d.setAlpha(130);
-		dialog.getWindow().setBackgroundDrawable(d);
-		
-		dialog.show();		
-	}
-		
-	/**
-	 * Central place for logging.
-	 * @param tag
-	 * @param text
-	 */
-	public static void Log(String tag, String msg) {
-		if (! DEBUG) return;
-		Log.d(tag, msg);
-	}
-	
-	public static void Log(String tag, String msg, String type) {
-		if (type == "e") { Log.e(tag, msg); }
-		else if (type == "w") { Log.w(tag, msg); }
-		else if (type == "i") { Log.i(tag, msg); }
-		else if (type == "d") { Log.d(tag, msg); }
-		else { Log.v(tag, msg); }
-	}
-	
-	public static enum RATE_TYPE {
-		TAX, /* 0-99, double  */
-		TIP  /* 0-99, integer */
-	}
+    private static boolean DEBUG = false;
+    public static String CONST_SharedPrefName = "@string/app_name"; // "BirthdayCalculator";
+    
+    public Util() {
+        // Get debug variable from values/strings.xml.
+    }
+    
+    public static void setDebugMode(boolean DebugMode) { DEBUG = DebugMode; }
+    public static boolean getDebugMode() { return DEBUG; }
+    
+    /**
+     * Display a dialog.
+     * 
+     * @param strTitle
+     * @param strErr
+     * @param me
+     */
+    public static void showDialog(String strTitle, String strErr, ActionBarActivity me) {
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(me); 
+        alertDialogBuilder.setTitle(strTitle);
+        alertDialogBuilder.setMessage(strErr);
+        alertDialogBuilder.setCancelable(false);
+        alertDialogBuilder.setPositiveButton("OK",
+                new DialogInterface.OnClickListener() {                        
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        //MainActivity.this.finish();
+                        dialog.cancel();
+                    }
+                });
+        
+        AlertDialog alertDialog = alertDialogBuilder.create();
+        
+        alertDialog.show();        
+    }
+    
+    /**
+     * This dialog needs a layout xml. To be implemented.
+     * 
+     * @param strTitle
+     * @param strErr
+     * @param me
+     */
+    public static void showDialog2(String strTitle, String strErr, ActionBarActivity me) {
+        final Dialog dialog = new Dialog(me);
+        //dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        
+        dialog.setTitle(strTitle);
+        dialog.setContentView(R.layout.activity_setting);
+        Drawable d = new ColorDrawable(Color.BLACK); 
+        d.setAlpha(130);
+        dialog.getWindow().setBackgroundDrawable(d);
+        
+        dialog.show();        
+    }
+        
+    /**
+     * Central place for logging.
+     * @param tag
+     * @param text
+     */
+    public static void Log(String tag, String msg) {
+        if (! DEBUG) return;
+        Log.d(tag, msg);
+    }
+    
+    public static void Log(String tag, String msg, String type) {
+        if (type == "e") { Log.e(tag, msg); }
+        else if (type == "w") { Log.w(tag, msg); }
+        else if (type == "i") { Log.i(tag, msg); }
+        else if (type == "d") { Log.d(tag, msg); }
+        else { Log.v(tag, msg); }
+    }
+    
+    public static enum RATE_TYPE {
+        TAX, /* 0-99, double  */
+        TIP  /* 0-99, integer */
+    }
 }
